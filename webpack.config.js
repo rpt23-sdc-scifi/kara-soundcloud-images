da2ee12fb05695236f66a3c8876fe4f88dd63cf5
Cam Christensen <camchristensenhr@gmail.com>
2020-10-12 20:09:36 -0700

    Updated CSS/Created BandProfile.jsx to export component to index for rendering/File clean-up

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,21 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env']
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
-    }]
+    ]
   }
 };
