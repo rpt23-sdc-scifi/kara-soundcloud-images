968b85a559d0eb3ee7d97bb5890b6a9beb44c43b
Cam Christensen <camchristensenhr@gmail.com>
2020-10-14 22:39:18 -0700

    Updated db/seed data, added delete func, made rendering dynamic

--- Test/test.js
+++ Test/test.js
@@ -8,15 +8,15 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('/GET band', () => {
-  it('Should return a band/artist with a bandId of 1', (done) => {
+  it('Should return a band/artist with a songId of 1', (done) => {
     chai.request(app)
       .get('/artistBio/1')
       .end((err, res) => {
         // console.log('Response: ', res.body);
         res.should.have.status(200);
         res.body.data.should.be.an('object');
-        res.body.data.bandId.should.be.a('number');
-        res.body.data.bandId.should.be.equal(1);
+        res.body.data.songId.should.be.a('number');
+        res.body.data.songId.should.be.equal(1);
         res.body.data.bandImageUrl.should.be.a('string');
         res.body.data.bandName.should.be.a('string');
         res.body.data.bandName.should.be.equal('Down Home Agita');
@@ -24,14 +24,14 @@ describe('/GET band', () => {
       });
   });
 
-  it('it should return an error if input bandId is 34', () => {
+  it('it should return an error if input songId is 101', () => {
     chai.request(app)
-      .get('/artistBio/34')
+      .get('/artistBio/101')
       .end((err, res) => {
         console.log(res.body);
         res.should.have.status(400);
         res.body.msg.should.be.a('string');
-        res.body.msg.should.be.equal('there is no band with id 34');
+        res.body.msg.should.be.equal('there is no band with songId 101');
 
       });
   });
