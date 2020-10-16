70f78234658d579d1c06db0778ccd6d4d1557023
Cam Christensen <camchristensenhr@gmail.com>
2020-10-16 13:35:40 -0700

    Dynamically rendering ALL components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import BandProfile from './BandProfile.jsx';
-// import css from '../../client/stylesheet.css';
+import css from '../../client/stylesheet.css';
 
 // ReactDOM.render(<BandProfile />, document.getElementById('artist-bio'));
 
