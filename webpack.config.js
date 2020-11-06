46677a7e76625d1256112a2ecf09bf162d1ba839
Cam Christensen <camchristensenhr@gmail.com>
2020-11-05 19:22:32 -0800

    Added compression logic

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = {
   entry: './client/src/index.jsx',
@@ -21,5 +23,20 @@ module.exports = {
       test: /\.css$/i,
       use: ['style-loader', 'css-loader']
     }]
-  }
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path][base].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+    })
+  ]
 };
